f9ba6afbb07dcae95398d0af7c9e26c6e0b40cc4
Matheus Henrique Passos de Oliveira <matheushtip@gmail.com>
2025-02-07 17:07:22 -0300

    now app.js makes a request for the github api

--- js/app.js
+++ js/app.js
@@ -1,1 +1,10 @@
 import { Octokit, App } from "https://esm.sh/octokit";
+const octokit = new Octokit({});
+
+octokit.request('GET /users/{username}/repos', {
+	username: 'matheusth',
+	headers: {
+		'X-Github-Api-Version': '2022-11-28'
+	}
+}).then(response => console.log(response));
+
