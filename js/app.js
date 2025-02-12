6038bfc9767a55bb18bc9a6f7c5fbac4b37c4ca5
Matheus Henrique Passos de Oliveira <matheushtip@gmail.com>
2025-02-12 13:47:43 -0300

    filtering the repositories to display

--- js/app.js
+++ js/app.js
@@ -3,10 +3,41 @@ const octokit = new Octokit({});
 
 document.body.onload = getProjects;
 
+let filterrepo = [
+	'alura-space',
+];
+
+function buildCards(repo) {
+	const projeto = document.createElement("section");
+	const projetoTitulo = document.createElement("h1");
+	const projetoDescricao = document.createElement("p");
+	const projetoImage = document.createElement("img");
+	const projetoTexto = document.createElement("div");
+	const projetoStars = document.createElement('p');
+
+	projeto.classList.add('projeto');
+	projetoTexto.classList.add('projetotexto');
+	projetoImage.classList.add('projetoimg');
+	projetoStars.classList.add('info');
+
+
+	projetoImage.src = `https://raw.githubusercontent.com/matheusth/${repo.name}/refs/heads/main/thumb.png`;
+	projetoTitulo.textContent = repo.name;
+	projetoDescricao.textContent = repo.description;
+	projetoStars.textContent = repo.stargazers_count;
+
+	projetoTexto.appendChild(projetoTitulo);
+	projetoTexto.appendChild(projetoDescricao);
+	projetoTexto.appendChild(projetoStars);
+	projeto.appendChild(projetoTexto);
+	projeto.appendChild(projetoImage);
+
+	document.getElementById("projetos").appendChild(projeto);
+}
+
 function getProjects() {
 	octokit.request('GET /users/{username}/repos', {
 		username: 'matheusth',
-		per_page: 5,
 		sort: 'updated',
 		headers: {
 			'X-Github-Api-Version': '2022-11-28'
@@ -14,27 +45,8 @@ function getProjects() {
 	}).then(
 		response => {
 			const repos = response.data;
-			repos.forEach(repo => {
-				const projeto = document.createElement("section");
-				const projetoTitulo = document.createElement("h1");
-				const projetoDescricao = document.createElement("p");
-				const projetoImage = document.createElement("img");
-				const projetoTexto = document.createElement("div");
-
-				projeto.classList.add('projeto');
-				projetoTexto.classList.add('projetotexto');
-				projetoImage.classList.add('projetoimg');
-
-				projetoImage.src = repo.owner.avatar_url;
-				projetoTitulo.textContent = repo.name;
-				projetoDescricao.textContent = repo.description;
-
-				projetoTexto.appendChild(projetoTitulo);
-				projetoTexto.appendChild(projetoDescricao);
-				projeto.appendChild(projetoTexto);
-				projeto.appendChild(projetoImage);
-
-				document.getElementById("projetos").appendChild(projeto);
+			repos.filter(repo => filterrepo.includes(repo.name)).forEach(repo => {
+				buildCards(repo);
 			});
 		});
 }
